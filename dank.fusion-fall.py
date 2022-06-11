839c6d472cbc941a20a25f1df7715f94ecb73b89
SirDankenstein <52797753+SirDank@users.noreply.github.com>
2022-06-11 18:59:26 +0530

    + Import Texture

--- dank.fusion-fall.py
+++ dank.fusion-fall.py
@@ -1,25 +1,50 @@
 # Note: dank.fusion-fall.py is meant to be run as an .exe by default, if you would like to execute the script, make the below changes...
-#       - uncomment the following line > filepath = os.path.dirname(__file__) # as .py
-#       - comment the following line > filepath = os.path.dirname(sys.argv[0]) # as .exe
-#       - if both of them were commented already, you do not have to change anything :)
+#       - set mode = "exe"
 
 from dankware import align, clr_banner, clr, cls
+import requests
+import time
 import json
 import sys
 import os
 
-'''if not os.path.exists("unitypack"):
+mode = "script"
+if mode == "script": filepath = os.path.dirname(__file__) # as .py
+else:filepath = os.path.dirname(sys.argv[0]) # as .exe
+os.chdir(filepath)
+if not os.path.exists("dank.ff"): os.mkdir("dank.ff")
+os.chdir("dank.ff")
+
+if mode == "script" and not os.path.exists("unitypack"):
     print(clr("\n  > ERROR: unitypack missing!",2))
     print(clr("  > Download it from: https://github.com/dongresource/UnityPackFF",2))
     print(clr("  > Exiting in 10s...",2))
-    time.sleep(10); sys.exit()'''
-
+    time.sleep(10); sys.exit()
 from unitypack.asset import Asset
+
+try:
+    from wand.image import Image
+    from unitypack.modding import import_texture
+except:
+    print(clr("\n  > ERROR: ImageMagick not installed!",2))
+    choice = input(clr("  > Download Now? [y/n]: "))
+    if choice.lower() == "y": print(clr("  > Downloading...")); download_name = "ImageMagick-7.1.0-37-Q16-HDRI-x64-dll.exe"; open(download_name,"wb").write(requests.get(f"https://download.imagemagick.org/ImageMagick/download/binaries/{download_name}").content); os.system(download_name)
+    else: sys.exit()
+    wait = input(clr("  > Hit [ENTER] after installing..."))
+    from unitypack.modding import import_texture
+
+# Dependencies for ffextract.py
+#import traceback
+#from io import BytesIO
+#from unitypack.export import OBJMesh
+#from unitypack.environment import UnityEnvironment
+#from collections import OrderedDict
+#from unitypack.object import ObjectPointer
+#from PIL import ImageOps
 
 def main():
 
     while True:
-
         banner = """
 
     ______  _______ __   _ _     _   _______ _______
@@ -32,38 +57,45 @@ def main():
         cls(); print(align(clr_banner(banner)))
         print(clr("  > [1] XDT Explorer"))
         print(clr("  > [0] Exit"))
-        
         choice = input(clr("\n  > Choice: "))
         if choice == "1": cls(); print(align(clr_banner(banner))); one()
         if choice == "0": cls(); sys.exit()
 
 def one():
     
-    exec("cab_path = input(clr('  > Drag and Drop Custom Asset Bundle: ')).replace('\"',''); index = int(input(clr('  > TableData Object Index [7]: ')))")
-    print(clr("\n  > f = open(cab_path, 'rb')"))
-    exec("f = open(cab_path, 'rb')")
+    cab_path = input(clr('  > Drag and Drop Custom Asset Bundle: ')).replace('"','')
+    index = int(input(clr('  > TableData Object Index: ')))
+    cab_name = str(cab_path.split('\\')[-1])
+    print(clr(f"\n  > f = open('{cab_path}', 'rb')"))
+    f = open(cab_path, 'rb')
     print(clr("  > tabledata = Asset.from_file(f)"))
-    exec("tabledata = Asset.from_file(f)")
-    print(clr("  > xdtdata = tabledata.objects[index].contents"))
-    exec("xdtdata = tabledata.objects[index].contents")
-    print(clr("\n  > Successfully loaded XDT data! You can now start running functions!\n"))
+    tabledata = Asset.from_file(f)
+    print(clr(f"  > xdtdata = tabledata.objects[{index}].contents"))
+    xdtdata = tabledata.objects[index].contents
+    print(clr("\n  > Pre-defined functions: save, dump-xdt\n")) # add extract
 
     while True:
         cmd = input(clr("  > ")); print()
-        if cmd == "exit": break
-        elif cmd == "save":
-            exec("tabledata.save(open(str(cab_path.split('\\\\')[-1]),'wb'))")
-        elif cmd == "dump-xdt":
-            exec('out = {}\nfor tname, table in xdtdata.items():\n    out[tname] = {}\n    try:\n        for dname, data in table.items(): out[tname][dname] = data\n    except: out[tname] = "<err>"\njson.dump(out, open("xdt.json", "w+"), indent=4)')
+        if cmd.lower() == "exit": break
+        elif cmd.lower() == "save":
+            tabledata.save(open(cab_name,'wb'))
+        elif cmd.lower() == "dump-xdt":
+            out = {}
+            for tname, table in xdtdata.items():
+                out[tname] = {}
+                try:
+                    for dname, data in table.items(): out[tname][dname] = data
+                except: out[tname] = "<err>"
+            json.dump(out, open("xdt.json", "w+"), indent=4)
+        #elif cmd.lower() == "extract":
+        #    counter = 1
+        #    while os.path.exists(f"{cab_name}_{counter}"): counter += 1
+        #    sys.argv.append(cab_path.replace(cab_name,'')); sys.argv.append(f"{cab_name}_{counter}")
+        #    exec(open(f"{os.path.dirname(__file__)}\\ffextract.py","r").read())
         else:
-            try: exec(cmd) #exec(f"print({cmd})")
+            try: exec(cmd)
             except Exception as err: print(clr(f"  > ERROR: {err}",2))
             print()
 
 if __name__ == "__main__":
-    filepath = os.path.dirname(__file__) # as .py
-    #filepath = os.path.dirname(sys.argv[0]) # as .exe
-    os.chdir(filepath)
-    if not os.path.exists("dank.ff"): os.mkdir("dank.ff")
-    os.chdir("dank.ff")
     main()
---
+++ ffextract.py
@@ -0,0 +1,116 @@
+import sys
+import os
+import traceback
+from io import BytesIO
+from unitypack.export import OBJMesh
+from unitypack.environment import UnityEnvironment
+from collections import OrderedDict
+from unitypack.object import ObjectPointer
+from PIL.ImageOps import flip
+
+def fixext(name):
+	tab = {'dds': 'png', 'nif': 'obj', 'kfm': 'obj', 'wav': 'ogg',
+		'mp3': 'ogg', 'jpg': 'png', 'psd': 'png', 'dds.asset': 'png',
+		'tga': 'png', 'tif': 'png', 'dds.mat': 'png', 'asset': 'png'}
+	for ext in tab.keys():
+		if name.endswith(ext): outname = name[:-len(ext)]; outname += tab[ext]; return outname
+	return name
+
+def write_to_file(path, contents, mode="w"):
+	if os.path.exists(path): return
+	with open(path, mode) as f: written = f.write(contents)
+
+def handle_texture(d, outpath):
+	try: image = d.image
+	except NotImplementedError: print("WARNING: Texture format not implemented. Skipping %r." % (filename)); return
+	if image is None: print("WARNING: %s is an empty image" % (filename)); return
+	img = flip(image); output = BytesIO(); img.save(output, format="png")
+	write_to_file(outpath, output.getvalue(), mode="wb")
+
+def handle_material(d, outpath):
+	texture_ref = d._obj['m_SavedProperties']['m_TexEnvs'][0][1]['m_Texture']
+	if texture_ref is not None: handle_texture(texture_ref.resolve(), outpath)
+
+def handle_mesh(d, outpath):
+	try: mesh_data = OBJMesh(d).export(); write_to_file(outpath, mesh_data, mode="w")
+	except NotImplementedError as e: print("WARNING: Could not extract %r (%s)" % (d, e))
+
+class GameobjectSearch:
+	def __init__(self):
+		self.seen = set()
+		self.meshes = []
+
+def gameobject_recurse(obj, srch):
+	if isinstance(obj, ObjectPointer) and obj.object not in srch.seen:
+		if obj.object.type == 'Mesh': srch.meshes.append(obj.resolve())
+		if obj.object.type == 'SkinnedMeshRenderer':
+			d = obj.resolve()
+			if d['m_Mesh'] is not None: srch.meshes.append(d['m_Mesh'].resolve())
+			return
+		elif obj.object.type == 'MeshFilter': return
+		srch.seen.add(obj.object); gameobject_recurse(obj.resolve(), srch)
+	d = obj
+	if not isinstance(d, OrderedDict):
+		try: d = obj._obj
+		except: pass
+	if not isinstance(d, OrderedDict) and not isinstance(d, list) and not isinstance(d, tuple): return
+	if type(d) == list:
+		for ent in d: gameobject_recurse(ent, srch)
+	elif type(d) == tuple:
+		for ent in d: gameobject_recurse(ent, srch)
+	else:
+		for k, v in d.items(): gameobject_recurse(v, srch)
+
+def handle_gameobject(obj, d, outpath):
+	srch = GameobjectSearch(); srch.seen.add(obj); gameobject_recurse(d, srch)
+	if len(srch.meshes) == 0: print('found {} meshes for {}'.format(len(srch.meshes), outpath))
+	i = 0
+	for mesh in srch.meshes:
+		outname = outpath
+		if len(srch.meshes) != 1: outname = outname.replace('.obj', ':{}.obj'.format(i))
+		handle_mesh(mesh, outname); i += 1
+
+def handle_object(obj, outpath):
+	d = obj.read()
+	if obj.type == "GameObject": handle_gameobject(obj, d, outpath)
+	elif obj.type == "Material": handle_material(d, outpath)
+	elif obj.type == "AudioClip": write_to_file(outpath, d.audio_data, mode="wb")
+	elif obj.type == "MovieTexture": write_to_file(outpath, d.movie_data, mode="wb")
+	elif obj.type == "Shader": write_to_file(outpath, d.script)
+	elif obj.type == "Font": write_to_file(outpath, d.data, mode="wb")
+	elif obj.type == "TextAsset": write_to_file(outpath, d.script, mode=mode)
+	elif obj.type == "Texture2D": handle_texture(d, outpath)
+	else: print('*** unhandled type', obj.type)
+
+def handle_assetbundle(asset, outdir):
+	if asset.objects[1].type_id != 142: return
+	cont = asset.objects[1].read()['m_Container']
+	for path, mtdt in cont:
+		os.makedirs(os.path.join(outdir, os.path.dirname(path)), mode=0o755, exist_ok=True)
+		outname = fixext(os.path.basename(path))
+		outpath = os.path.join(outdir, os.path.dirname(path), outname)
+		if os.path.exists(outpath): print('** {} exists, skipping...'.format(outpath)); continue
+		try: handle_object(mtdt['asset'].object, outpath)
+		except Exception as e:
+			if isinstance(e, KeyboardInterrupt): raise e
+			print('** error while handling object {}'.format(outpath))
+			traceback.print_exc(file=sys.stdout)
+
+def main(path, outdir):
+	environment.base_path = path; files = os.listdir(path)
+	for ent in os.listdir(path):
+		abspath = os.path.join(path, ent)
+		if os.path.isdir(abspath): files.remove(ent); files.extend([os.path.join(ent, f) for f in os.listdir(abspath)])
+	for cas in files:
+		name = os.path.basename(cas).lower()
+		if not (name.startswith('customassetbundle') or name.startswith('buildplayer')): continue
+		print('* opening', name); print('* {} assetbundles open'.format(len(environment.files)))
+		try: asset = environment.get_asset_by_filename(cas); handle_assetbundle(asset, outdir)
+		except Exception as e:
+			if isinstance(e, KeyboardInterrupt): break
+			print('* error while handling assetbundle {}'.format(cas)); traceback.print_exc(file=sys.stdout)
+
+if __name__ == '__main__':
+	if len(sys.argv) != 3: print('usage: ffextract.py indir outdir'); sys.exit(1)
+	environment = UnityEnvironment()
+	main(sys.argv[1], sys.argv[2])
